3fd4129871bc17e8319075cc49a69f1c641b60d1
Eldhose ER <eldhoseer16@gmail.comgit config --global user.email eldhoseer16@gmail.comgit config --global user.name Eldhose>
2024-01-12 17:50:04 +0530

    updates

--- src/components/User/userHome/Userhome.jsx
+++ src/components/User/userHome/Userhome.jsx
@@ -58,7 +58,7 @@ const Dispatch=useDispatch()
             />
 
             <img
-              src="../../../../public/Images/cbr650r.png"
+              src="/assets/Images/Registration-Certificate.jpg"
               alt=""
               className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 mt-4 lg:mt-8 "
             />
