faed57d9202db881f6f85614ad347fb5a43fa737
Eldhose ER <eldhoseer16@gmail.comgit config --global user.email eldhoseer16@gmail.comgit config --global user.name Eldhose>
2024-01-12 19:05:05 +0530

    lll

--- src/components/User/userHome/Userhome.jsx
+++ src/components/User/userHome/Userhome.jsx
@@ -5,7 +5,7 @@ import { useState, useEffect } from "react";
 import Footer from "../Footer/Footer";
 import { useDispatch } from "react-redux";
 import { isbookinpagefalse } from "../../../redux/NavbarSlice";
-import Homeimage from "../../../assets/Images/cbr650r-homebanner_new5.jpg"
+import Homeimage from "../../../assets/license.jpg"
 
 export default function Userhome() {
   const [bike, setbike] = useState([]);
@@ -77,15 +77,16 @@ const Dispatch=useDispatch()
         <UserNav />
         <div className="pt-[5rem] lg:pt-[6rem] lg:block">
           <img
-              src="assets/Images/honda-unicorn-abc-right-side-view_360x240.webp"
+              // src="assets/Images/honda-unicorn-abc-right-side-view_360x240.webp"
 
-// src={Homeimage}
+src={Homeimage}
             // src="../../../../public/Images/cbr650r-homebanner_new5.jpg"
             alt=""
             className="mx-auto"
           />
           <img
             // src="'../../../../public/Images/cbr650r.png"
+          src={Homeimage}
             alt=""
             className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 mt-4 lg:mt-8 pb-[35rem] md:pb-[18rm] "
           />
