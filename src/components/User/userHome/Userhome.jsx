dc1f1113b4e32078601e5817f8934ba3497dfe49
Eldhose ER <eldhoseer16@gmail.comgit config --global user.email eldhoseer16@gmail.comgit config --global user.name Eldhose>
2024-01-12 17:54:03 +0530

    new pic

--- src/components/User/userHome/Userhome.jsx
+++ src/components/User/userHome/Userhome.jsx
@@ -58,7 +58,7 @@ const Dispatch=useDispatch()
             />
 
             <img
-              src="/assets/Images/Registration-Certificate.jpg"
+              src="\assets\Images\honda-unicorn-abc-right-side-view_360x240.webp"
               alt=""
               className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 mt-4 lg:mt-8 "
             />
